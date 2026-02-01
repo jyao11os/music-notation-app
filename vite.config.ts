a2a8442888965468fcada0d6edb7b992f0744b68
jyao11os <71713724+jyao11os@users.noreply.github.com>
2026-02-01 16:00:54 +0800

    upgrade Node.js version to 20

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-// import electron from 'vite-plugin-electron';
-// import renderer from 'vite-plugin-electron-renderer';
+import electron from 'vite-plugin-electron';
+import renderer from 'vite-plugin-electron-renderer';
 
 export default defineConfig({
   plugins: [
     react(),
-    // electron([
-    //   {
-    //     entry: 'electron/main.ts',
-    //   },
-    // ]),
-    // renderer(),
+    electron([
+      {
+        entry: 'electron/main.ts',
+      },
+    ]),
+    renderer(),
   ],
   server: {
     port: 5173,
